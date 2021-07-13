2753b1fa422d94aca0d20ae3ac051e7be7dc5bf2
Jakub Westa <jakub.westa@gmail.com>
2021-07-13 15:11:52 +0200

    Added example recipes

--- src/main/java/net/lightning_recipes/mixins/LightningEntityMixin.java
+++ src/main/java/net/lightning_recipes/mixins/LightningEntityMixin.java
@@ -9,7 +9,6 @@ import net.minecraft.block.LightningRodBlock;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.LightningEntity;
 import net.minecraft.inventory.SimpleInventory;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
@@ -33,34 +32,25 @@ public class LightningEntityMixin {
         BlockPos blockForRecipePos;
         BlockState blockForRecipeState;
 
-        System.out.println(blockState.getBlock());
-
         if (blockState.isOf(Blocks.LIGHTNING_ROD)) {
             System.out.println("1");
             blockForRecipePos = pos.offset(((Direction)blockState.get(LightningRodBlock.FACING)).getOpposite());
             blockForRecipeState = world.getBlockState(blockForRecipePos);
             checkForRecipes(blockForRecipeState, world, blockForRecipePos);
-            System.out.println("2");
         }
     }
 
     private static void checkForRecipes(BlockState blockState, World world, BlockPos blockPos) {
-        System.out.println("3");
-        System.out.println(world.isClient);
-
         SimpleInventory inventory = new SimpleInventory(new ItemStack(blockState.getBlock().asItem()));
         Optional<LightningRecipe> match = world.getRecipeManager().getFirstMatch(LightningRecipe.Type.INSTANCE, inventory, world);
-        System.out.println("4");
 
         if (match.isPresent()) {
             LightningRecipes.debugMsg("Found recipe for " + blockState.getBlock().toString());
             ItemStack outputStack = match.get().getOutput();
             Block blockResult = Block.getBlockFromItem(outputStack.getItem());
             world.setBlockState(blockPos, blockResult.getDefaultState());
-            System.out.println("5");
         } else {
             LightningRecipes.debugMsg("No recipe found for " + blockState.getBlock().toString());
-            System.out.println("6");
         }
 
     }
